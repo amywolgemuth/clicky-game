35999d3ebb9f72028442750d6346ab2c5df2e3f1
Amy Wolf <awolgemuth@trinisys.loc>
2019-09-27 18:29:43 -0500

    added MemoryCard and MemoryGameContainer to src components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 import Jumbotron from "./components/Jumbotron";
-import './App.css';
+import MemoryGameContainer from "./components/MemoryGameContainer";
+import "./App.css";
+
 
 const App = () => (
   <div className="container-fluid mainContainer">
     <Jumbotron />
+    <MemoryGameContainer /> 
   </div>
 );
 
